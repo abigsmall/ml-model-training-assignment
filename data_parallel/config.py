41e2068c344bfcd11e2dd7d88fb95fcaeea08e90
Justin Hong <abigsmall@gmail.com>
2025-08-25 07:08:53 +0000

    add data parallel code

---
+++ data_parallel/config.py
@@ -0,0 +1,25 @@
+# Define hyperparameters
+do_data_parallel = False
+
+per_device_batch_size = 1
+dataloader_num_workers = 16
+learning_rate = 1e-4
+epochs = 1
+
+# Path PyTorch should save model to
+tv_model_path = '/home/ubuntu/ml-model-training-assignment/model_data'
+
+# Path Imagenette is at
+imagenette_train_path = '/home/ubuntu/ml-model-training-assignment/imagenette/train'
+imagenette_test_path = '/home/ubuntu/ml-model-training-assignment/imagenette/val'
+
+train_data_size = 100
+test_data_size = 20
+
+device = 'cuda'
+
+# Flag used to simulate limited memory. Set to 1.0 if you wish to use 100% memory on each device
+memory_limit = 1.0
+
+# Only use the specified devices
+visible_devices = [0,1,2,3]
