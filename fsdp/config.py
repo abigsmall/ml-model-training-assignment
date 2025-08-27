76fa834621aa656d2ce6666cfe29c29fe0f24d5f
Justin Hong <abigsmall@gmail.com>
2025-08-27 02:40:54 -0700

    add initial fsdp work

---
+++ fsdp/config.py
@@ -0,0 +1,19 @@
+# Define hyperparameters
+per_device_batch_size = 1
+dataloader_num_workers = 2
+learning_rate = 1e-4
+epochs = 1
+
+# Path PyTorch should save model to
+tv_model_path = '/home/ubuntu/ml-model-training-assignment/model_data'
+
+# Path Imagenette is at
+imagenette_train_path = '/home/ubuntu/imagenette2/train'
+imagenette_test_path = '/home/ubuntu/imagenette2/val'
+
+# -1 means full dataset
+train_data_size = -1
+test_data_size = -1
+
+# Flag used to simulate limited memory. Set to 1.0 if you wish to use 100% memory on each device
+memory_limit = 1.0
